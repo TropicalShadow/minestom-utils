1ab42e9f8119426d4f5be61e05284acb342b4730
TropicalShadow <me@tesseract.club>
2026-06-03 20:38:31 +0100

    feat(instance): set chunk supplier to use LightingChunk in DummyInstance

--- src/test/java/club/tesseract/minestom/test/instance/DummyInstance.java
+++ src/test/java/club/tesseract/minestom/test/instance/DummyInstance.java
@@ -2,6 +2,7 @@ package club.tesseract.minestom.test.instance;
 
 import club.tesseract.minestom.utils.instance.dimension.FullBrightDimension;
 import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
 
 import java.util.UUID;
@@ -9,6 +10,8 @@ import java.util.UUID;
 public class DummyInstance extends InstanceContainer {
     public DummyInstance() {
         super(UUID.randomUUID(), FullBrightDimension.getInstance().getRegistryKeyDimension());
+
+        setChunkSupplier(LightingChunk::new);
 
         setGenerator((unit) ->{
             unit.modifier().fillHeight(0, 69, Block.SANDSTONE);
